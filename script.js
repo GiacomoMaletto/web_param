9f3874dacff1bcd2600fa74521a914b30788d56b
GiacomoMaletto <malettogiacomo6@gmail.com>
2023-02-21 14:13:12 +0100

    from example

--- script.js
+++ script.js
@@ -1,15 +1,81 @@
-const canvas = document.getElementById('canvas');
-const context = canvas.getContext('2d');
+const canvas = document.getElementById('drawing-area');
+const canvasContext = canvas.getContext('2d');
+const clearButton = document.getElementById('clear-button');
+const state = {
+    mousedown: false
+};
 
-window.addEventListener('resize', resizeCanvas, false);
+const radius = 10;
 
-function resizeCanvas() {
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
+canvas.addEventListener('mousedown', handleWritingStart);
+canvas.addEventListener('mousemove', handleWritingInProgress);
+canvas.addEventListener('mouseup', handleDrawingEnd);
+canvas.addEventListener('mouseout', handleDrawingEnd);
 
-    context.strokeStyle = "#00ff00";
-    context.rect(20, 20, 150, 100);
-    context.stroke();
+canvas.addEventListener('touchstart', handleWritingStart);
+canvas.addEventListener('touchmove', handleWritingInProgress);
+canvas.addEventListener('touchend', handleDrawingEnd);
+
+clearButton.addEventListener('click', handleClearButtonClick);
+
+function handleWritingStart(event) {
+    event.preventDefault();
+
+    const mousePos = getMousePositionOnCanvas(event);
+
+    clearCanvas();
+
+    canvasContext.beginPath();
+    canvasContext.arc(mousePos.x, mousePos.y, radius, 0, 2 * Math.PI, false);
+    canvasContext.fillStyle = 'green';
+    canvasContext.fill();
+    canvasContext.lineWidth = 5;
+    canvasContext.strokeStyle = '#003300';
+    canvasContext.stroke();
+
+    state.mousedown = true;
 }
 
-resizeCanvas();
+function handleWritingInProgress(event) {
+    event.preventDefault();
+
+    if (state.mousedown) {
+        const mousePos = getMousePositionOnCanvas(event);
+
+        clearCanvas();
+
+        canvasContext.beginPath();
+        canvasContext.arc(mousePos.x, mousePos.y, radius, 0, 2 * Math.PI, false);
+        canvasContext.fillStyle = 'green';
+        canvasContext.fill();
+        canvasContext.lineWidth = 5;
+        canvasContext.strokeStyle = '#003300';
+        canvasContext.stroke();
+    }
+}
+
+function handleDrawingEnd(event) {
+    event.preventDefault();
+
+    state.mousedown = false;
+}
+
+function handleClearButtonClick(event) {
+    event.preventDefault();
+
+    clearCanvas();
+}
+
+function getMousePositionOnCanvas(event) {
+    const clientX = event.clientX || event.touches[0].clientX;
+    const clientY = event.clientY || event.touches[0].clientY;
+    const { offsetLeft, offsetTop } = event.target;
+    const canvasX = clientX - offsetLeft;
+    const canvasY = clientY - offsetTop;
+
+    return { x: canvasX, y: canvasY };
+}
+
+function clearCanvas() {
+    canvasContext.clearRect(0, 0, canvas.width, canvas.height);
+}
